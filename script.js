0fba6ba44be22ae7014e0a936a8fd673be310fb7
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-04-26 22:29:01 +0330

    cart part 2 script

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ cart.classList.add("active");
 cartClose.addEventListener("click",()=>{
     cart.classList.remove("active");
 })
+
+const addCartBtn=document.querySelectorAll(".add-cart");
+addCartBtn.forEach(button=>{
+    button.addEventListener("click",event=>{
+        const productBox=event.target.closest(".product-box");
+        addToCart(productBox);
+    })
+})
+const cartContent=document.querySelector(".cart-content");
+const addToCart = productBox =>{
+    const pImg=productBox.querySelector("img").src;
+    const pTitle=productBox.querySelector(".product-title").textContent;
+    const pPrice=productBox.querySelector(".price").textContent;
+
+    const cartBox=document.createElement("div");
+    cartBox.classList.add("cart-box");
+    cartBox.innerHTML=`
+     <img src="${pImg}" class="cart-img">
+                <div class="cart-detail">
+                    <h2 class="cart-product-title">${pTitle}</h2>
+                    <span class="cart-price">${pPrice}</span>
+                    <div class="cart-quantity">
+                        <button id="decrement">-</button>
+                        <span class="number">1</span>
+                        <button id="increment">+</button>
+                    </div>
+                </div>
+                <i class="fa fa-trash cart-remove"></i>`;
+                cartContent.appendChild(cartBox);
+}
