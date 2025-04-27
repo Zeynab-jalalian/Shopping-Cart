d73ab8194bfd0bc6771df5c2e6af5c0d5679a5b6
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-04-27 09:56:51 +0330

    cart remove

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-const cartIcon=document.querySelector("#icon-cart");
-const cart=document.querySelector(".cart");
-const cartClose=document.querySelector("#cart-close");
-cartIcon.addEventListener("click",()=>{
-cart.classList.add("active");
+const cartIcon = document.querySelector("#icon-cart");
+const cart = document.querySelector(".cart");
+const cartClose = document.querySelector("#cart-close");
+cartIcon.addEventListener("click", () => {
+    cart.classList.add("active");
 })
-cartClose.addEventListener("click",()=>{
+cartClose.addEventListener("click", () => {
     cart.classList.remove("active");
 })
 
 
 
 
-const addCartBtn=document.querySelectorAll(".add-cart");
-addCartBtn.forEach(button=>{
-    button.addEventListener("click",event=>{
-        const productBox=event.target.closest(".product-box");
+const addCartBtn = document.querySelectorAll(".add-cart");
+addCartBtn.forEach(button => {
+    button.addEventListener("click", event => {
+        const productBox = event.target.closest(".product-box");
         addToCart(productBox);
     })
 })
 
 
 
-const cartContent=document.querySelector(".cart-content");
-const addToCart = productBox =>{
-    const pImg=productBox.querySelector("img").src;
-    const pTitle=productBox.querySelector(".product-title").textContent;
-    const pPrice=productBox.querySelector(".price").textContent;
+const cartContent = document.querySelector(".cart-content");
+const addToCart = productBox => {
+    const pImg = productBox.querySelector("img").src;
+    const pTitle = productBox.querySelector(".product-title").textContent;
+    const pPrice = productBox.querySelector(".price").textContent;
 
 
-  const cartItems=cartContent.querySelectorAll(".cart-img");
-  for (let item of cartItems){
-    if(item.src=== pImg){
-        alert("This item is already in the cart.")
-        return;
+    const cartItems = cartContent.querySelectorAll(".cart-img");
+    for (let item of cartItems) {
+        if (item.src === pImg) {
+            alert("This item is already in the cart.")
+            return;
+        }
     }
-  }
 
 
 
-    const cartBox=document.createElement("div");
+    const cartBox = document.createElement("div");
     cartBox.classList.add("cart-box");
-    cartBox.innerHTML=`
+    cartBox.innerHTML = `
      <img src="${pImg}" class="cart-img">
                 <div class="cart-detail">
                     <h2 class="cart-product-title">${pTitle}</h2>
@@ -52,5 +52,9 @@ const addToCart = productBox =>{
                     </div>
                 </div>
                 <i class="fa fa-trash cart-remove"></i>`;
-                cartContent.appendChild(cartBox);
+    cartContent.appendChild(cartBox);
+    cartBox.querySelector(".cart-remove").addEventListener("click",()=>{
+        cartBox.remove();
+    })
 }
+
