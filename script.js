bd7e2e0c022eb26070b8b40766c2a3e67c975f60
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-04-27 09:55:08 +0330

    script

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const cartIcon=document.querySelector("#icon-cart");
 const cart=document.querySelector(".cart");
 const cartClose=document.querySelector("#cart-close");
-
 cartIcon.addEventListener("click",()=>{
 cart.classList.add("active");
 })
 cartClose.addEventListener("click",()=>{
     cart.classList.remove("active");
 })
+
+
+
 
 const addCartBtn=document.querySelectorAll(".add-cart");
 addCartBtn.forEach(button=>{
@@ -16,11 +18,25 @@ addCartBtn.forEach(button=>{
         addToCart(productBox);
     })
 })
+
+
+
 const cartContent=document.querySelector(".cart-content");
 const addToCart = productBox =>{
     const pImg=productBox.querySelector("img").src;
     const pTitle=productBox.querySelector(".product-title").textContent;
     const pPrice=productBox.querySelector(".price").textContent;
+
+
+  const cartItems=cartContent.querySelectorAll(".cart-img");
+  for (let item of cartItems){
+    if(item.src=== pImg){
+        alert("This item is already in the cart.")
+        return;
+    }
+  }
+
+
 
     const cartBox=document.createElement("div");
     cartBox.classList.add("cart-box");
